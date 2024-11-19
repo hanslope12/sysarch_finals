e2712ed4f2787f0c50e8faf9c5e5fdf7f12a91ae
hanslope12 <91108047+hanslope12@users.noreply.github.com>
2024-11-19 17:28:20 +0800

    Changed table position

--- sysarch_finals_back/index.js
+++ sysarch_finals_back/index.js
@@ -12,12 +12,6 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 connectDB();
 
-app.get('/getAnime', (req, res) => {
-    Anime.find()
-    .then(Anime => res.json(Anime))
-    .catch(err => res.json(err))
-})
-
 app.use(cors());
 app.use(express.json());
 
--- sysarch_finals_front/src/pages/Home.jsx
+++ sysarch_finals_front/src/pages/Home.jsx
@@ -1,9 +1,41 @@
 import {Link} from 'react-router-dom';
 import { Navbar } from './navbar';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 export function Home(){
+    const [animeList, setAnimeList] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/animes')
+      .then(response => setAnimeList(response.data))
+      .catch(err => console.log(err))
+  }, [])
+
     return(
         <>
         <Navbar/>
+        <table className="table">
+        <thead>
+          <tr>
+            <th>Anime_ID</th>
+            <th>Name</th>
+            <th>Genres</th>
+            <th>Ranked</th>
+          </tr>
+        </thead>
+        <tbody>
+          {animeList.map(anime => (
+            <tr key={anime.anime_id}>
+              <td>{anime.anime_id}</td>
+              <td>{anime.Name}</td>
+              <td>{anime.Genres}</td>
+              <td>{anime.Ranked}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
         <h1>This is the Home page</h1>
         
         </>
