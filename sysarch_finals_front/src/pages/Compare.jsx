fe382942271b3586954cf424f3280800439259cc
cheesy-lasagna <nicolepecho9174@gmail.com>
2024-11-24 16:44:02 +0800

    print kaggle data

--- sysarch_finals_front/src/pages/Compare.jsx
+++ sysarch_finals_front/src/pages/Compare.jsx
@@ -17,7 +17,52 @@ export function Compare(){
     return(
         <>
         <Navbar/>
-        <h1>This is the Home page</h1>
+        {/*print kaggle */}
+        <h1>KAGGLE DATA</h1>
+        <div align="left">
+        <table className="table">
+        <thead>
+          <tr>
+            <th>Anime_ID</th>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Genres</th>
+            <th>Ranked</th>
+          </tr>
+        </thead>
+        <tbody>
+          {animeList.map(anime => (
+            <tr key={anime.anime_id}>
+              <td>{anime.anime_id}</td>
+              <td><img src={anime.Image} alt={anime.anime_id} /></td>
+              <td>{anime.Name}</td>
+              <td>{anime.Genres}</td>
+              <td>{anime.Ranked}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+        </div>
+
+        {/*print MAL */}
+        <h1>MAL DATA</h1>
+        <div align="right">
+        <table className="table">
+        <thead>
+          <tr>
+            <th>Anime_ID</th>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Genres</th>
+            <th>Ranked</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+        </div>
+        
+        <h1>This is the Compare page</h1>
         </>
     )
 }
